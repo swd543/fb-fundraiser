45f27ed2e6827e46360f648beb70e1a14592dd5f
Swapneel Datta <swd543@gmail.com>
2021-02-24 11:41:04 +0100

    Check why login button not initialized on github pages

--- spa/src/services/init-facebook-sdk.js
+++ spa/src/services/init-facebook-sdk.js
@@ -2,35 +2,6 @@ const { REACT_APP_FACEBOOK_APP_ID } = process.env
 
 export function initFacebookSdk() {
     return new Promise(resolve => {
-        // wait for facebook sdk to initialize before starting the react app
-        window.fbAsyncInit = function () {
-            window.FB.init({
-                appId: REACT_APP_FACEBOOK_APP_ID,
-                cookie: true,
-                xfbml: true,
-                version: 'v9.0'
-            });
-
-            // window.FB.getLoginStatus(response => console.log(response))
-
-            // auto authenticate with the api if already logged in with facebook
-            // window.FB.getLoginStatus(({ authResponse }) => {
-            //     if (authResponse) {
-            //         // accountService.apiAuthenticate(authResponse.accessToken).then(resolve);
-            //         fetch()
-            //         const response = axios.post(`${baseUrl}/authenticate`, { accessToken });
-            //         const account = response.data;
-            //         accountSubject.next(account);
-            //         startAuthenticateTimer();
-            //         return account;
-            //     } else {
-            //         resolve();
-            //     }
-            // });
-            resolve()
-        };
-
-        // load facebook sdk script
         (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) { return; }
@@ -42,5 +13,18 @@ export function initFacebookSdk() {
             js.crossorigin = 'anonymous'
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
+
+        // wait for facebook sdk to initialize before starting the react app
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: REACT_APP_FACEBOOK_APP_ID,
+                cookie: true,
+                xfbml: true,
+                status: true,
+                version: 'v9.0'
+            });
+            console.log('loaded')
+            resolve('Finished loading SDK')
+        };
     });
 }

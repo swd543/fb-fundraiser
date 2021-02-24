1236018de3e277150edfc9bdf1a9afc999a7c227
Swapneel Datta <swd543@gmail.com>
2021-02-24 10:32:13 +0100

    Added text service for new pages

--- spa/src/Main.js
+++ spa/src/Main.js
@@ -7,33 +7,40 @@ import Contact from "./pages/Contact";
 import Exit from "./pages/Exit";
 import Home from "./pages/Home";
 import Nonprofit from "./pages/Nonprofit";
+import { PrivacyPolicy } from "./pages/PrivacyPolicy";
+import { Terms } from "./pages/Terms";
 import Token from "./pages/Token";
 
-class Main extends Component {
-
-  render() {
-    return (
-      <HashRouter>
-        <React.Fragment>
-          <ul className="header">
-            <li><NavLink exact to="/" style={{pointerEvents:'none'}}>Welcome</NavLink></li>
-            <li><NavLink to="/token" style={{pointerEvents:'none'}}>Token access</NavLink></li>
-            <li><NavLink to="/nonprofit" style={{pointerEvents:'none'}}>Choose fundraiser</NavLink></li>
-            <li><NavLink to="/contact" style={{pointerEvents:'none'}}>Contact</NavLink></li>
-            <li style={{float:'right'}}><NavLink to="/exit">Exit</NavLink></li>
+export const Main = (_props) => {
+  return (
+    <HashRouter>
+      <div className="container">
+        <ul className="header">
+          <li><NavLink exact to="/" style={{ pointerEvents: 'none' }}>Welcome</NavLink></li>
+          <li><NavLink to="/token" style={{ pointerEvents: 'none' }}>Token access</NavLink></li>
+          <li><NavLink to="/nonprofit" style={{ pointerEvents: 'none' }}>Choose fundraiser</NavLink></li>
+          <li><NavLink to="/contact" style={{ pointerEvents: 'none' }}>Contact</NavLink></li>
+          <li style={{ float: 'right' }}><NavLink to="/exit">Exit</NavLink></li>
+        </ul>
+        <div className="content" style={{ border: '1px dashed green' }}>
+          <Route exact path="/" component={Home} />
+          <Route path="/token" component={Token} />
+          <Route path="/nonprofit" component={Nonprofit} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/exit" component={Exit} />
+          <Route path="/confirm" component={Confirmation} />
+          <Route exact path="/terms" component={Terms} />
+          <Route exact path="/privacy" component={PrivacyPolicy} />
+        </div>
+        <div style={{ backgroundColor: 'beige', bottom: 0, marginTop: 'auto', display: 'flex', padding: 32, justifyContent: 'center' }}>
+          <ul className="banner">
+            <li><NavLink exact to="/terms">Terms and conditions</NavLink></li>
+            <li><NavLink exact to="/privacy">Privacy Policy</NavLink></li>
           </ul>
-          <div className="content" style={{border:'1px dashed green'}}>
-            <Route exact path="/" component={Home}/>
-            <Route path="/token" component={Token}/>
-            <Route path="/nonprofit" component={Nonprofit}/>
-            <Route path="/contact" component={Contact}/>
-            <Route path="/exit" component={Exit}/>
-            <Route path="/confirm" component={Confirmation}/>
-          </div>
-        </React.Fragment>
-      </HashRouter>
-    );
-  }
+        </div>
+      </div>
+    </HashRouter>
+  )
 }
 
 export default Main;
